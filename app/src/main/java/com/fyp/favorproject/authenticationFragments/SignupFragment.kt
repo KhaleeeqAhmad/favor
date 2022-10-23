60ddfe195f46852ecdbc31a5a04a95e80654e8b6
Khaleeq Ahmad <107875965+KhaleeeqAhmad@users.noreply.github.com>
2022-10-23 12:21:25 +0500

    Login Signup Done Automatically Login Done User Data Saved in Database

--- app/src/main/java/com/fyp/favorproject/authenticationFragments/SignupFragment.kt
+++ app/src/main/java/com/fyp/favorproject/authenticationFragments/SignupFragment.kt
@@ -1,12 +1,5 @@
 package com.fyp.favorproject.authenticationFragments
-
-<<<<<<< HEAD
-=======
-
-import android.content.ContentValues.TAG
->>>>>>> e92599ac15c86a732a38b6553770c990ad024ffa
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,23 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.fyp.favorproject.R
 import com.fyp.favorproject.databinding.FragmentSignupBinding
-import com.fyp.favorproject.model.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
-
-
 
 class SignupFragment : Fragment() {
-    private lateinit var binding: FragmentSignupBinding
-
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSignupBinding
     private lateinit var database: FirebaseDatabase
+
+
 
 
     override fun onCreateView(
@@ -41,8 +27,22 @@ class SignupFragment : Fragment() {
     ): View {
         binding = FragmentSignupBinding.inflate(inflater)
 
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        binding = FragmentSignupBinding.inflate(inflater)
+
+        isUserAlreadyLoggedIn()
+
         binding.btnSignup.setOnClickListener {
-         findNavController().navigate(R.id.action_signupFragment_to_mainActivity)
+           if(binding.etUserNameSignup.text.toString().isBlank() ||
+               binding.etEmailSignup.text.toString().isBlank() ||
+               binding.etPasswordSignup.text.toString().isBlank()) {
+               Toast.makeText(context , "Empty credentials", Toast.LENGTH_SHORT)
+                   .show()
+           } else {
+               signUp()
+           }
         }
         binding.btnSignIn.setOnClickListener {
             findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
@@ -50,31 +50,28 @@ class SignupFragment : Fragment() {
 
         return binding.root
     }
-        // Initialize Firebase Auth
-        auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance()
-        binding = FragmentSignupBinding.inflate(inflater)
-
-        binding.btnSignup.setOnClickListener() {
-            var name = binding.etUserNameSignup.text.toString()
-            var email = binding.etEmailSignup.text.toString().trim()
-            var password = binding.etPasswordSignup.toString()
-
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val newUser = User(name, email, password)
-                        val id = it.result.user?.uid as String
-                        database.reference.child("User").child(id).setValue(newUser)
-                        Toast.makeText(context, "UserDataSaved", Toast.LENGTH_SHORT).show()
-
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", it.exception)
-                        Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    }
-                }
+    private fun signUp() {
+        val name = binding.etUserNameSignup.text.toString()
+        val email = binding.etEmailSignup.text.toString().trim()
+        val password = binding.etPasswordSignup.text.toString()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+            if (it.isSuccessful) {
+                val newUser = com.fyp.favorproject.userData.User(name, email, password)
+                val id = it.result.user?.uid as String
+                database.reference.child("User").child(id).setValue(newUser)
+                Toast.makeText(context, "UserDataSaved", Toast.LENGTH_SHORT)
+                    .show()
+                findNavController().navigate(R.id.action_signupFragment_to_mainActivity)
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
-        return binding.root
+    }
+    private fun isUserAlreadyLoggedIn() {
+        if (FirebaseAuth.getInstance().currentUser != null){
+            findNavController().navigate(R.id.action_signupFragment_to_mainActivity)
+        }
     }
 }
---
+++ app/src/main/java/com/fyp/favorproject/userData/User.kt
@@ -0,0 +1,7 @@
+package com.fyp.favorproject.userData
+
+data class User (
+    val name: String,
+    val email: String,
+    val password: String
+        )
